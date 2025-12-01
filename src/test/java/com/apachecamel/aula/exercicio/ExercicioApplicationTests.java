30469e84c4ba8ba01a6778019dcd4baeb2cd8769
LucasJordao <lucaswill12@hotmail.com>
2025-12-01 00:04:19 -0300

    feat: init project

---
+++ src/test/java/com/apachecamel/aula/exercicio/ExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apachecamel.aula.exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
